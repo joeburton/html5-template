4ee865e270c1db1363f3d09c665b784eefe2144c
Joe Burton <joeburton@gmail.com>
2013-03-18 19:05:07 +0000

    commit js changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,44 @@
 
+
+/* if console.log undefined create it */
 if (typeof console == "undefined") {
-    this.console = { log: function (msg) { alert(msg); } };
+    this.console = { 
+		log: function (msg) { 
+			alert(msg); 
+		}
+	};
 }
 
-function somewhereSpecial(e) {
-
-	console.log('somewhere special', e.x, e.y, this);
-
+/* add a new object to the global window
+if (typeof frog == "undefined") {
+    this.frog = { 
+		parts: function (msg) { 
+			alert(msg); 
+		}
+	};
 }
+
+frog.parts("mainly legs");
+*/
+ 
+ 
+/*
+push document.getElementById to $ 
 
 function $(id) { 
 	return document.getElementById(id); 
 }
 
+function somewhereSpecial(e) {
+	console.log("x:position: " + e.x + " y:position: " + e.y + " what's this's value: " + this);
+}
+
 var somewhere = $("somewhere");
 
 somewhere.addEventListener("click", somewhereSpecial, false)
+*/
 
-/*
+
 $('*[data-selection-id]').each(function() {
 	
     var id = $(this).data('selection-id');
@@ -37,12 +58,13 @@ var mapStuff = {
 };
 
 mapStuff.details();
+/*
 */
 
 
 /*
 Constructor Pattern
-*/
+
 function Chocolate () {
 
 	this.colour = "dark";
@@ -63,11 +85,11 @@ var chocolateCake = new Chocolate();
 chocolateBar.price = "20";
 
 console.log(chocolateBar.toString());
-
+*/
 
 /*
 Object literal pattern
-*/
+
 var chocolate = {
 
 	colour: "dark",
@@ -82,3 +104,59 @@ var chocolate = {
 };
 
 chocolate.returnDetails();
+*/
+
+function Numbers () {
+
+	this.o = {
+		first: "first",
+		second: "second",
+		third: "third",
+		fourth: "fourth",
+		fith: "fith",
+		somethingdiff: ["a", "b", "c"],
+		someobj: {"object": "really"}
+	}
+	
+}
+
+Numbers.prototype.item = function() {
+	return ("something");
+}
+
+var myNumbers = new Numbers();
+
+var someText = myNumbers.item();
+
+console.log("text: ", someText, myNumbers, myNumbers.item);
+
+var o = {
+	first: "first",
+	second: "second",
+	third: "third",
+	fourth: "fourth",
+	fith: "fith",
+	somethingdiff: ["a", "b", "c"],
+	someobj: {"object": "really"}
+};
+
+if (typeof Object.prototype.clone === "undefined") {
+	Object.prototype.clone = function() {
+		alert('cloning begins');
+	}
+}
+
+for (name in o) {
+	
+	if (o.hasOwnProperty(name)) {
+		console.log(name, o[name]);
+	}
+	
+}
+
+console.log("Object: ", Object.clone);
+
+
+
+
+
